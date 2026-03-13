5efdea9f3bfd3f0b3ac9a31a77c5dc01017c619c
Nilton Brito <nilton.brito@outlook.com>
2026-03-13 12:44:15 -0300

    :sparkles: feat: Commit - Criado a classe ENUM StatusContaPagar e a classe ContaPagar.

---
+++ src/main/java/com/bandampla/lojavirtual/enums/StatusContaPagar.java
@@ -0,0 +1,21 @@
+package com.bandampla.lojavirtual.enums;
+
+public enum StatusContaPagar {
+
+	COBRANCA("Pagar"), VENCIDA("Vencida"), ABERTA("Aberta"), QUITADA("Quitada"), RENEGOCIADA("Renegociada");
+
+	private String descricao;
+
+	private StatusContaPagar(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	@Override
+	public String toString() {
+		return this.descricao;
+	}
+}
---
+++ src/main/java/com/bandampla/lojavirtual/model/ContaPagar.java
@@ -0,0 +1,149 @@
+package com.bandampla.lojavirtual.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.ConstraintMode;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.bandampla.lojavirtual.enums.StatusContaPagar;
+
+@Entity
+@Table(name = "conta_pagar")
+@SequenceGenerator(name = "seq_conta_pagar", sequenceName = "seq_conta_pagar", allocationSize = 1, initialValue = 1)
+public class ContaPagar implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_conta_pagar")
+	private Long id;
+
+	@Column(nullable = false)
+	private String descricao;
+
+	@Enumerated(EnumType.STRING)
+	private StatusContaPagar status;
+
+	private BigDecimal valorTotal;
+
+	private BigDecimal valorDesconto;
+
+	@Temporal(TemporalType.DATE)
+	private Date dataVencimento;
+
+	@Temporal(TemporalType.DATE)
+	private Date dataPagamento;
+
+	@ManyToOne(targetEntity = Pessoa.class)
+	@JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
+	private Pessoa pessoa;
+
+	@ManyToOne(targetEntity = Pessoa.class)
+	@JoinColumn(name = "pessoa_fornecedor_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fornecedor_fk"))
+	private Pessoa pessoaFornecedor;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public StatusContaPagar getStatus() {
+		return status;
+	}
+
+	public void setStatus(StatusContaPagar status) {
+		this.status = status;
+	}
+
+	public BigDecimal getValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(BigDecimal valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+	public BigDecimal getValorDesconto() {
+		return valorDesconto;
+	}
+
+	public void setValorDesconto(BigDecimal valorDesconto) {
+		this.valorDesconto = valorDesconto;
+	}
+
+	public Date getDataVencimento() {
+		return dataVencimento;
+	}
+
+	public void setDataVencimento(Date dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+
+	public Date getDataPagamento() {
+		return dataPagamento;
+	}
+
+	public void setDataPagamento(Date dataPagamento) {
+		this.dataPagamento = dataPagamento;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	public Pessoa getPessoaFornecedor() {
+		return pessoaFornecedor;
+	}
+
+	public void setPessoaFornecedor(Pessoa pessoaFornecedor) {
+		this.pessoaFornecedor = pessoaFornecedor;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ContaPagar other = (ContaPagar) obj;
+		return Objects.equals(id, other.id);
+	}
+}
