0c793566569a8930daf95fa0470aafa1d7262381
Nilton Brito <nilton.brito@outlook.com>
2026-03-13 14:07:09 -0300

    :sparkles: feat: Commit - Criação da Classe CupomDesconto

---
+++ src/main/java/com/bandampla/lojavirtual/model/CupomDesconto.java
@@ -0,0 +1,99 @@
+package com.bandampla.lojavirtual.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "cupom_desconto")
+@SequenceGenerator(name = "seq_cupom_desconto", sequenceName = "seq_cupom_desconto", allocationSize = 1, initialValue = 1)
+public class CupomDesconto implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_cupom_desconto")
+	private Long id;
+
+	@Column(nullable = false)
+	private String codigoDescricao;
+
+	private BigDecimal valorRealDesconto;
+
+	private BigDecimal valorPorcentagemDesconto;
+
+	@Temporal(TemporalType.DATE)
+	private Date dataValidade;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCodigoDescricao() {
+		return codigoDescricao;
+	}
+
+	public void setCodigoDescricao(String codigoDescricao) {
+		this.codigoDescricao = codigoDescricao;
+	}
+
+	public BigDecimal getValorRealDesconto() {
+		return valorRealDesconto;
+	}
+
+	public void setValorRealDesconto(BigDecimal valorRealDesconto) {
+		this.valorRealDesconto = valorRealDesconto;
+	}
+
+	public BigDecimal getValorPorcentagemDesconto() {
+		return valorPorcentagemDesconto;
+	}
+
+	public void setValorPorcentagemDesconto(BigDecimal valorPorcentagemDesconto) {
+		this.valorPorcentagemDesconto = valorPorcentagemDesconto;
+	}
+
+	public Date getDataValidade() {
+		return dataValidade;
+	}
+
+	public void setDataValidade(Date dataValidade) {
+		this.dataValidade = dataValidade;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CupomDesconto other = (CupomDesconto) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
