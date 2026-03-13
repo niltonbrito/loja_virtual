96c05f4a2a83d149c48ad45954834c2c131c1cef
Nilton Brito <nilton.brito@outlook.com>
2026-03-13 13:08:29 -0300

    :zap: perf: Commit - Melhoria na classe ENUM StatusContaPagar

--- src/main/java/com/bandampla/lojavirtual/enums/StatusContaPagar.java
+++ src/main/java/com/bandampla/lojavirtual/enums/StatusContaPagar.java
@@ -2,7 +2,7 @@ package com.bandampla.lojavirtual.enums;
 
 public enum StatusContaPagar {
 
-	COBRANCA("Pagar"), VENCIDA("Vencida"), ABERTA("Aberta"), QUITADA("Quitada"), RENEGOCIADA("Renegociada");
+	COBRANCA("Pagar"), VENCIDA("Vencida"), ABERTA("Aberta"), QUITADA("Quitada"), RENEGOCIADA("Renegociada"), ALUGUEL("Aluguel"), FUNCIONARIO("Funcionário"), DESPESA("Despesa");
 
 	private String descricao;
 
