1514327fa534169844b794145fb8c42519a18616
Nilton Brito <nilton.brito@outlook.com>
2026-03-13 13:10:06 -0300

    :sparkles: feat: Commit - Criado a classe ENUM StatusFormaPagamento

---
+++ src/main/java/com/bandampla/lojavirtual/enums/StatusFormaPagamento.java
@@ -0,0 +1,21 @@
+package com.bandampla.lojavirtual.enums;
+
+public enum StatusFormaPagamento {
+
+	PIX("Pix"), AVISTA("À Vista"), CARTAODEBITO("Cartão de Débito"), CARTAOCREDITO("Cartão de Credito"), BOLETO("Boleto"), BOLETO15("Boleto 15 dias"), BOLETO30("Boleto 30 Dias");
+
+	private String descricao;
+
+	private StatusFormaPagamento(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	@Override
+	public String toString() {
+		return this.descricao;
+	}
+}
